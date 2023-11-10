9b423f58f90a0889b14a0640f43b67069316d93d
arthurmeb <120094553+arthurmeb@users.noreply.github.com>
2023-11-10 02:12:55 +0000

    reset

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-// app.js
-const express = require('express');
-const cors = require('cors');
-const { currentDaily } = require('./scheduleDaily');
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const {currentDaily } = require('./scheduleDaily')
 
-const app = express();
+// Define API
 
-console.log('Initial daily:', currentDaily)
+app.use(cors({ origin: 'http://localhost:8081' }))
 
-app.use(cors({ origin: 'http://localhost:8081' }));
-
-app.use(express.static('assets'));
+app.use(express.static('assets'))
 
 app.get('/api/daily', (req, res) => {
-    console.log('Data sent:', currentDaily);
-    res.json(currentDaily);
-});
+    console.log('Data sent.', currentDaily)
+    res.json(currentDaily)
+})
+
+console.log('Initial daily is:', currentDaily)
+
+const serverPort = process.env.PORT || 3000
+
+app.listen(serverPort, () => {
+    console.log(`Server is listening on port ${serverPort}`);
+  })
--- scheduleDaily.js
+++ scheduleDaily.js
@@ -1,7 +1,6 @@
 // scheduleDaily.js
 const schedule = require('node-schedule');
 const { daily } = require('./imagesCycle');
-const {io} = require('./server');
 
 let index = 0;
 let currentDaily = daily[index];
@@ -10,14 +9,13 @@ const cycleDaily = () => {
     index++;
     currentDaily = daily[index];
     console.log('Cycled daily. New daily is:', currentDaily);
-    io.emit('dailyUpdated', currentDaily)
 };
 
 const eventTime = new schedule.RecurrenceRule();
 eventTime.tz = 'Etc/GMT-0';
 eventTime.hour = 1;
-eventTime.minute = 45;
-eventTime.second = 10;
+eventTime.minute = 57;
+eventTime.second = 55;
 
 const scheduledEvent = schedule.scheduleJob(eventTime, cycleDaily);
 
