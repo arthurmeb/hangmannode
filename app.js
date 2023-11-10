7ebdd5a9e32187fcd9ba11c3e09868f0ebfea073
arthurmeb <120094553+arthurmeb@users.noreply.github.com>
2023-11-10 06:43:52 +0000

    getting there...

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
+
 
 
 const schedule = require('node-schedule');
@@ -14,37 +17,60 @@ const io = require('socket.io')(http, {
     }
 });
 
+
+const filePath = path.join(__dirname, 'thedaily.json');
+
+
+
 let index = 0;
 let currentDaily = daily[index];
-let ummm
 
 const cycleDaily = () => {
+
     index++;
     newDaily = daily[index];
     console.log('Cycled daily. New daily is:', newDaily);
 
+    const jsonString = JSON.stringify(newDaily, null, 2); // The third argument (2) is for indentation
+    fs.writeFileSync(filePath, jsonString, 'utf8');
+
+    console.log('File updated with newDaily:', jsonString);
     // Emit the updated daily value to all connected clients
-    io.emit('dailyUpdated', newDaily);
+    io.emit('dailyUpdated', jsonString);
 };
 
 
 // Socket.io connection handling
 io.on('connection', (socket) => {
     console.log('A user connected');
-    socket.emit('idk', currentDaily);
-});
+    
+  try {
+    // Read the JavaScript file and parse its content
+    const fileContent = fs.readFileSync(filePath, 'utf8');
 
+    console.log('File content is', fileContent)
 
-// Send the first day's value ONCE when the server is first
-io.emit('dayOne', currentDaily)
+    const jsonDaily = JSON.parse(fileContent);
 
-console.log('This is after io.emit...')
+    console.log('Json content is', jsonDaily)
+
+    // Emit the data to the connected client
+
+    socket.emit('idk', jsonDaily);
+
+  } catch (error) {
+    console.error('Error reading or parsing the file:', error);
+};
+})
+
+// setInterval(() => {cycleDaily()}, 5000)
+
 
 const eventTime = new schedule.RecurrenceRule();
 eventTime.tz = 'Etc/GMT-0';
-eventTime.hour = 4;
-eventTime.minute = 44;
-eventTime.second = 55;
+eventTime.hour = 6;
+eventTime.minute = 42;
+eventTime.second = 20;
 
 const scheduledEvent = schedule.scheduleJob(eventTime, cycleDaily);
 
@@ -56,8 +82,6 @@ app.get('/api/daily', (req, res) => {
     console.log('Data sent.', currentDaily);
     res.json(currentDaily);
 });
-
-console.log('First ever daily:', currentDaily)
 
 const serverPort = process.env.PORT || 3000;
 
