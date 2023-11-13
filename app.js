fe51c307f7781be644fbb9c80e76e1b6227b0aa8
arthurmeb <120094553+arthurmeb@users.noreply.github.com>
2023-11-13 01:34:32 +0000

    all annotated

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ const path = require('path');
 
 
 const schedule = require('node-schedule');
+// Grab daily object from imagesCycle.js
 const { daily } = require('./imagesCycle');
 const http = require('http').createServer(app);
+// Configure socketio with cors for images and server client communiation
 const io = require('socket.io')(http, {
     cors: {
         origin: 'http://localhost:8080',
@@ -17,30 +19,44 @@ const io = require('socket.io')(http, {
     }
 });
 
+// Define filepath where the daily will be written to and read from (impromptu database)
 
 const filePath = path.join(__dirname, 'thedaily.json');
 
+// Define function to cycle each daily
 
 const cycleDaily = () => {
 
+        // Read the file's content and wrap in variable
+
     const fileContent = fs.readFileSync(filePath, 'utf8');
 
+    // Create a javascript object from the json string
+
     const jsonDaily = JSON.parse(fileContent);
+
+    // Take the current daily's day value and use it as an index for which day of the cycle we are on. [critical for cycling]
+
     let index = jsonDaily.day
     index++;
     newDaily = daily[index];
     console.log('Cycled daily. New daily is:', newDaily, 'Index is', index)
 
+    // Create json string from the new daily object and write it to the daily file
+
     const jsonString = JSON.stringify(newDaily, null, 2); // The third argument (2) is for indentation
     fs.writeFileSync(filePath, jsonString, 'utf8');
 
     console.log('File updated with newDaily:', jsonString);
+
     // Emit the updated daily value to all connected clients
+
     io.emit('dailyUpdated', jsonString);
 };
 
 
-// Socket.io connection handling
+// Socket.io when a client connects
+
 io.on('connection', (socket) => {
     console.log('A user connected');
     
@@ -52,7 +68,7 @@ io.on('connection', (socket) => {
 
     console.log('Starting content is', jsonDaily)
 
-    // Emit the data to the connected client
+    // Emit the data to the connected client so they know the current daily
 
     socket.emit('idk', jsonDaily);
 
@@ -61,15 +77,19 @@ io.on('connection', (socket) => {
 };
 })
 
+// Define the time at which daily should cycle
 
 const eventTime = new schedule.RecurrenceRule();
 eventTime.tz = 'Etc/GMT-0';
-eventTime.hour = 6;
-eventTime.minute = 55;
-eventTime.second = 20;
+eventTime.hour = 0;
+eventTime.minute = 0;
+eventTime.second = 0;
+
+// Run schedule event at midnight everyday to trigger daily cycle
 
 const scheduledEvent = schedule.scheduleJob(eventTime, cycleDaily);
 
+// Allow promptImages folder to serve static files to client
 
 app.use('/assets/promptImages', (req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
